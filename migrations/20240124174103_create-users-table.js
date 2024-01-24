38c48970f95b65b30dea9ab71d2b822d0fdec210
awhr95 <awhr95@gmail.com>
2024-01-24 18:47:00 +0000

    Add knex migrations for initial database tables users & opportunities

---
+++ migrations/20240124174103_create-users-table.js
@@ -0,0 +1,13 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return;
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {};
