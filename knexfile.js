cfbcef466821fe3601b5b6fcc1028e72ca7a72e5
awhr95 <awhr95@gmail.com>
2024-01-24 17:36:01 +0000

    Add folder set up & initial database set up

---
+++ knexfile.js
@@ -0,0 +1,9 @@
+module.exports = {
+  client: "mysql2",
+  connection: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_LOCAL,
+    user: process.env.DB_LOCAL_USER,
+    password: process.env.DB_LOCAL_PASSWORD,
+  },
+};
