89e5daf8b932768bc9a506a6f6e7ac165cad0f94
awhr95 <awhr95@gmail.com>
2024-01-30 15:12:47 +0000

    Refactored user routes to controllers

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -26,4 +26,64 @@ const getAllUsers = async (_req, res) => {
   }
 };
 
-module.exports = { getAllUsers };
+const registerUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send("Please enter the required fields.");
+  }
+
+  const hashedPassword = bcrypt.hashSync(password, 6);
+
+  const newUser = {
+    user_name: "",
+    email,
+    bio: "",
+    current_number_of_opportunities: 0,
+    total_opportunities: 0,
+    password: hashedPassword,
+    first_name: "",
+    last_name: "",
+  };
+
+  try {
+    const users = await knex("users").insert(newUser);
+    const response = await knex("users").where({ id: users[0] }).first();
+    return res.status(201).json(response);
+  } catch (error) {
+    return res.status(400).json({ message: "Failed registration" });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send("Please enter the required fields");
+  }
+
+  try {
+    const user = await knex("users").where({ email: email }).first();
+
+    const passwordCorrect = bcrypt.compareSync(password, user.password);
+
+    if (!passwordCorrect) {
+      return res.status(400).send("Invalid password");
+    }
+
+    const token = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "24h",
+      }
+    );
+
+    return res.json({ token });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "Failed login" });
+  }
+};
+
+module.exports = { getAllUsers, registerUser, loginUser };
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -4,68 +4,16 @@ const knex = require("knex")(require("../knexfile"));
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
-const { getAllUsers } = require("../controllers/usersControllers");
+const {
+  getAllUsers,
+  registerUser,
+  loginUser,
+} = require("../controllers/usersControllers");
 
 router.route("/").get(getAllUsers);
 
-router.post("/register", async (req, res) => {
-  const { email, password } = req.body;
+router.route("/register").post(registerUser);
 
-  if (!email || !password) {
-    return res.status(400).send("Please enter the required fields.");
-  }
-
-  const hashedPassword = bcrypt.hashSync(password, 6);
-
-  const newUser = {
-    user_name: "",
-    email,
-    bio: "",
-    current_number_of_opportunities: 0,
-    total_opportunities: 0,
-    password: hashedPassword,
-    first_name: "",
-    last_name: "",
-  };
-
-  try {
-    const users = await knex("users").insert(newUser);
-    const response = await knex("users").where({ id: users[0] }).first();
-    return res.status(201).json(response);
-  } catch (error) {
-    return res.status(400).json({ message: "Failed registration" });
-  }
-});
-
-router.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-
-  if (!email || !password) {
-    return res.status(400).send("Please enter the required fields");
-  }
-
-  try {
-    const user = await knex("users").where({ email: email }).first();
-
-    const passwordCorrect = bcrypt.compareSync(password, user.password);
-
-    if (!passwordCorrect) {
-      return res.status(400).send("Invalid password");
-    }
-
-    const token = jwt.sign(
-      { id: user.id, email: user.email },
-      process.env.JWT_SECRET,
-      {
-        expiresIn: "24h",
-      }
-    );
-
-    return res.json({ token });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ message: "Failed login" });
-  }
-});
+router.route("/login").post(loginUser);
 
 module.exports = router;
